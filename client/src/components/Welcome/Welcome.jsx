2feca95853eae2e5056260f75a258c0ac953eb9d
pablo pelardas <pablo.pelardas@gmail.com>
2022-06-19 21:54:25 -0300

    feat(client/getHistoryQuery): query to get user's operation history

--- client/src/components/Welcome/Welcome.jsx
+++ client/src/components/Welcome/Welcome.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
-import { useGetUserQuery, useGetOperationsByUserQuery } from '../../slices/api/userApiSlice'
+import { useGetUserQuery, useGetLastOperationsQuery } from '../../slices/api/userApiSlice'
 
 const Welcome = ({ currentUser }) => {
   console.log(currentUser)
   useGetUserQuery(currentUser.id)
-  useGetOperationsByUserQuery(currentUser.id)
+  useGetLastOperationsQuery(currentUser.id)
   return (
     <section>
-      <h1>Bienvenido {currentUser?.name}</h1>
+      <h1>Welcome {currentUser?.name}</h1>
       <div>
         <p>Balance: {currentUser?.balance}</p>
       </div>
       <div>
         <p>Last Operations: </p>
         <ul>
-          {currentUser?.last_operations?.slice(0, 10).map((operation, index) => (
-            <li key={index}>{operation.mount} | {operation.type} | {operation.concept}</li>
+          {currentUser?.last_operations?.map((operation, index) => (
+            <li key={index}>{operation.mount} | {operation.type} | {operation.concept} | {operation.createdAt.slice(0, 10)}</li>
           ))}
         </ul>
       </div>
--- client/src/slices/api/userApiSlice.js
+++ client/src/slices/api/userApiSlice.js
@@ -9,13 +9,13 @@ export const userApiSlice = apiSlice.injectEndpoints({
         try {
           const { data } = await queryFulfilled
           console.log(data)
-          dispatch(setCredentials({ user: { name: data.name, operations: data.operations, balance: data.balance } }))
+          dispatch(setCredentials({ user: { name: data.name, balance: data.balance } }))
         } catch (err) {
           console.log('Error fetching user')
         }
       }
     }),
-    getOperationsByUser: builder.query({
+    getLastOperations: builder.query({
       query: (id) => `/operation/user/${id}`,
       async onQueryStarted (_, { dispatch, queryFulfilled }) {
         try {
@@ -27,11 +27,24 @@ export const userApiSlice = apiSlice.injectEndpoints({
           console.log('Error fetching user')
         }
       }
+    }),
+    getAllOperations: builder.query({
+      query: (id, type) => `/operation/history/${id}?type=${type}`,
+      async onQueryStarted (_, { dispatch, queryFulfilled }) {
+        try {
+          const { data } = await queryFulfilled
+          dispatch(setCredentials({ user: { all_operations: data } }))
+        } catch (err) {
+          console.log(err)
+          console.log('Error fetching user')
+        }
+      }
     })
   })
 })
 
 export const {
   useGetUserQuery,
-  useGetOperationsByUserQuery
+  useGetLastOperationsQuery,
+  useGetAllOperationsQuery
 } = userApiSlice
