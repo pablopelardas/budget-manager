690a457c0ddeddb789d83e9c3f59f53e9b2de709
pablo pelardas <pablo.pelardas@gmail.com>
2022-06-21 09:16:16 -0300

    refactor(client/components): relocate container components to container folders

---
+++ client/src/containers/OperationManager/OperationManager.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import OperationInput from '../../components/OperationInput/OperationInput'
+
+const OperationManager = () => {
+  return (
+    <div>
+      <h1>Soy el Operation Manager</h1>
+      <OperationInput />
+    </div>
+  )
+}
+
+export default OperationManager
