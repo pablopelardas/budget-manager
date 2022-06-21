1152754321e1ca83c55a5c4cdcb397ef33a89a98
pablo pelardas <pablo.pelardas@gmail.com>
2022-06-21 09:16:46 -0300

    feat(client/home): add OperationManager component

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import Welcome from '../../components/Welcome/Welcome'
-import Login from '../../components/Login/Login'
+import Welcome from '../../containers/Welcome/Welcome'
+import Login from '../../containers/Login/Login'
 import { useSelector } from 'react-redux'
 import { selectCurrentToken, selectCurrentUser } from '../../slices/app/userSlice'
+import OperationManager from '../../containers/OperationManager/OperationManager'
 
 const Home = () => {
   const currentUser = useSelector(selectCurrentUser)
@@ -10,9 +11,14 @@ const Home = () => {
   React.useEffect(() => {
 
   }, [currentUser])
-  const content = currentToken
-    ? <Welcome currentUser={currentUser} />
-    : <Login />
+  const content = !currentToken
+    ? <Login />
+    : (
+      <>
+        <Welcome currentUser={currentUser} />
+        <OperationManager />
+      </>
+      )
 
   return (
     <main>
